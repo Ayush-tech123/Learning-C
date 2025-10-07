b3968d58a5e476a99bde2a75b6259c3cab217eda
Ayush Sharma <ayush.sharma@avantika.edu.in>
2025-10-07 23:08:55 +0530

    Finding cube root of Integers

---
+++ Chapter-1/Cube.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+
+    int num;
+
+    printf("Enter a number ");
+    scanf("%d", &num);
+
+    printf("The cube root is %d", num * num * num);
+
+    return 0;
+}
