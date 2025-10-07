6745c81222d3245058c423e02b61f42a68222d80
Ayush Sharma <ayush.sharma@avantika.edu.in>
2025-10-07 22:41:58 +0530

    Finding the sum of two variables via input

---
+++ Chapter-1/Sum_of_two_variable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    int a, b;
+
+    printf("Enter a ");
+    scanf("%d", &a);
+
+    printf("Enter b ");
+    scanf("%d", &b);
+
+    printf("The sum is a + b = %d", a + b);
+
+    return 0;
+}
